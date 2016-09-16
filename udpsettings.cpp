4951ac1b52e56a181c5658fd34c27fef7db9e9e6
Arew <noobterr@yandex.ru>
2016-09-16 14:40:18 +0300

    Bug fix

--- udpsettings.cpp
+++ udpsettings.cpp
@@ -5,8 +5,12 @@
 
 //pimpl
 struct UdpSettings::Data {
-    uint port = 6000;
-    QString ip = "192.168.0.90";
+    uint port1 = 6000;
+    QString ip1 = "192.168.0.93";
+    uint port2 = 6000;
+    QString ip2 = "192.168.0.94";
+    uint port3 = 8080;
+    QString ip3 = "192.168.0.90";
 };
 
 
@@ -14,27 +18,62 @@ UdpSettings::UdpSettings(QWidget *parent):
     QWidget(parent),
     data(new Data)
 {
-    ipEdit = new QLineEdit(this);
-    portEdit = new QLineEdit(this);
+    ip1Edit = new QLineEdit(this);
+    port1Edit = new QLineEdit(this);
+    ip2Edit = new QLineEdit(this);
+    port2Edit = new QLineEdit(this);
+    ip3Edit = new QLineEdit(this);
+    port3Edit = new QLineEdit(this);
 
-    QLabel* ipLabel = new QLabel("Ip: ", this);
-    QLabel* portLabel = new QLabel("Port: ", this);
+    QLabel* ipLabel = new QLabel("Ip №1: ", this);
+    QLabel* portLabel = new QLabel("Port №1: ", this);
 
     //buddy setup
-    ipLabel->setBuddy(ipEdit);
-    portLabel->setBuddy(portEdit);
+    ipLabel->setBuddy(ip1Edit);
+    portLabel->setBuddy(port1Edit);
 
     QGroupBox* groupBox = new QGroupBox("Udp settings", this);
 
     QHBoxLayout* lay = new QHBoxLayout();
 
     lay->addWidget(ipLabel);
-    lay->addWidget(ipEdit);
+    lay->addWidget(ip1Edit);
     lay->addSpacing(10);
     lay->addWidget(portLabel);
-    lay->addWidget(portEdit);
+    lay->addWidget(port1Edit);
 
-    groupBox->setLayout(lay);
+    //group2
+    QLabel* ip2Label = new QLabel("Ip №2: ", this);
+    QLabel* port2Label = new QLabel("Port №2: ", this);
+    ip2Label->setBuddy(ip2Edit);
+    port2Label->setBuddy(port2Edit);
+
+    QHBoxLayout* lay2 = new QHBoxLayout();
+    lay2->addWidget(ip2Label);
+    lay2->addWidget(ip2Edit);
+    lay2->addSpacing(10);
+    lay2->addWidget(port2Label);
+    lay2->addWidget(port2Edit);
+
+    //group3
+    QLabel* ip3Label = new QLabel("Ip №3: ", this);
+    QLabel* port3Label = new QLabel("Port №3: ", this);
+    ip3Label->setBuddy(ip3Edit);
+    port3Label->setBuddy(port3Edit);
+
+    QHBoxLayout* lay3 = new QHBoxLayout();
+    lay3->addWidget(ip3Label);
+    lay3->addWidget(ip3Edit);
+    lay3->addSpacing(10);
+    lay3->addWidget(port3Label);
+    lay3->addWidget(port3Edit);
+
+    QVBoxLayout* vLay = new QVBoxLayout();
+    vLay->addLayout(lay);
+    vLay->addLayout(lay2);
+    vLay->addLayout(lay3);
+
+    groupBox->setLayout(vLay);
 
     QHBoxLayout* groupLay = new QHBoxLayout();
     groupLay->addWidget(groupBox);
@@ -44,11 +83,19 @@ UdpSettings::UdpSettings(QWidget *parent):
     setWindowFlags(Qt::ToolTip | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
 
     //set default values
-    ipEdit->setText(data->ip);
-    portEdit->setText(QString::number(data->port));
+    ip1Edit->setText(data->ip1);
+    port1Edit->setText(QString::number(data->port1));
+    ip2Edit->setText(data->ip2);
+    port2Edit->setText(QString::number(data->port2));
+    ip3Edit->setText(data->ip3);
+    port3Edit->setText(QString::number(data->port3));
 
-    QObject::connect(ipEdit, SIGNAL(textChanged(QString)), SLOT(ipTextEdit(QString)));
-    QObject::connect(portEdit, SIGNAL(textChanged(QString)), SLOT(portTextEdit(QString)));
+    QObject::connect(ip1Edit, SIGNAL(textChanged(QString)), SLOT(ip1TextEdit(QString)));
+    QObject::connect(port1Edit, SIGNAL(textChanged(QString)), SLOT(port1TextEdit(QString)));
+    QObject::connect(ip2Edit, SIGNAL(textChanged(QString)), SLOT(ip2TextEdit(QString)));
+    QObject::connect(port2Edit, SIGNAL(textChanged(QString)), SLOT(port2TextEdit(QString)));
+    QObject::connect(ip3Edit, SIGNAL(textChanged(QString)), SLOT(ip3TextEdit(QString)));
+    QObject::connect(port3Edit, SIGNAL(textChanged(QString)), SLOT(port3TextEdit(QString)));
 }
 
 UdpSettings::~UdpSettings()
@@ -56,22 +103,62 @@ UdpSettings::~UdpSettings()
     delete data;
 }
 
-uint UdpSettings::port() const
+uint UdpSettings::port1() const
 {
-    return data->port;
+    return data->port1;
 }
 
-QString UdpSettings::ip() const
+QString UdpSettings::ip1() const
 {
-    return data->ip;
+    return data->ip1;
 }
 
-void UdpSettings::ipTextEdit(const QString &str)
+uint UdpSettings::port2() const
 {
-    data->ip = str;
+    return data->port2;
 }
 
-void UdpSettings::portTextEdit(const QString &str)
+QString UdpSettings::ip2() const
 {
-    data->port = str.toUInt();
+    return data->ip2;
+}
+
+uint UdpSettings::port3() const
+{
+    return data->port3;
+}
+
+QString UdpSettings::ip3() const
+{
+    return data->ip3;
+}
+
+void UdpSettings::ip1TextEdit(const QString &str)
+{
+    data->ip1 = str;
+}
+
+void UdpSettings::port1TextEdit(const QString &str)
+{
+    data->port1 = str.toUInt();
+}
+
+void UdpSettings::ip2TextEdit(const QString &str)
+{
+    data->ip2 = str;
+}
+
+void UdpSettings::port2TextEdit(const QString &str)
+{
+    data->port2 = str.toUInt();
+}
+
+void UdpSettings::ip3TextEdit(const QString &str)
+{
+    data->ip3 = str;
+}
+
+void UdpSettings::port3TextEdit(const QString &str)
+{
+    data->port3 = str.toUInt();
 }
--- udpsettings.h
+++ udpsettings.h
@@ -15,19 +15,35 @@ private:
     struct Data;
     Data* data;
 
-    //gui
-    QLineEdit* ipEdit;
-    QLineEdit* portEdit;
+    //options 1
+    QLineEdit* ip1Edit;
+    QLineEdit* port1Edit;
+
+    //options 2
+    QLineEdit* ip2Edit;
+    QLineEdit* port2Edit;
+
+    //options 3
+    QLineEdit* ip3Edit;
+    QLineEdit* port3Edit;
 
 public:
 
     //interface
-    uint port() const;
-    QString ip() const;
+    uint port1() const;
+    QString ip1() const;
+    uint port2() const;
+    QString ip2() const;
+    uint port3() const;
+    QString ip3() const;
 
 private slots:
-    void ipTextEdit(const QString& str);
-    void portTextEdit(const QString& str);
+    void ip1TextEdit(const QString& str);
+    void port1TextEdit(const QString& str);
+    void ip2TextEdit(const QString& str);
+    void port2TextEdit(const QString& str);
+    void ip3TextEdit(const QString& str);
+    void port3TextEdit(const QString& str);
 };
 
 #endif // UDPSETTINGS_H
